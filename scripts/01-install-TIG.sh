bcff991d088f89424246092b73e9fd43b3eb9801
Shan Desai <shantanoo.desai@gmail.com>
2020-07-13 15:01:21 +0200

    bugfix(TIG): use .deb/.rpm packages for Distros
    - Should address #2

--- scripts/01-install-TIG.sh
+++ scripts/01-install-TIG.sh
@@ -8,12 +8,12 @@
 #   DESCRIPTION:  Bash Script to install Telegraf/InfluxDB/Grafana Latest Stable version on a machine
 # 
 #       OPTIONS:  ---
-#  REQUIREMENTS:  env.vars file generated from 00-generate-cert.sh to configure InfluxDB
+#  REQUIREMENTS:  env.vars file generated from 00-generate-cert.sh to configure InfluxDB, Grafana
 #          BUGS:  ---
 #         NOTES:  Not tested on RHEL, CentOS, Fedora
 #        AUTHOR:  Shantanoo Desai, shantanoo.desai@gmail.com, des@biba.uni-bremen.de
 #       COMPANY:  BIBA - Bremer Institut fuer Produktion und Logistik GmbH
-#       VERSION:  0.3
+#       VERSION:  0.4
 #       CREATED:  07/07/20 14:06:48 CEST
 #      REVISION:  ---
 #===============================================================================
@@ -22,6 +22,121 @@ ROOT_UID=0
 E_NOTROOT=85
 SCRIPTSDIR=`pwd`
 ENVFILE="env.vars"
+
+function download_influxdb (distro) {
+
+	case distro in
+	"Debian"|"Ubuntu")
+		echo -e "Downloading Debian Package for InfluxDB v1.8\n"
+		wget --continue -P /tmp/influxdb/ "https://dl.influxdata.com/influxdb/releases/influxdb_1.8.0_amd64.deb"
+		
+		if [[ -f "/tmp/influxdb/influxdb_1.8.0_arm64.deb" ]]; then
+			echo -e "Installing InfluxDB v1.8.0\n"
+			dpkg -i /tmp/influxdb/influxdb_1.8.0_arm64.deb
+		else
+			echo -e "Error while downloading InfluxDB Debian Package\n"
+			exit 3
+		fi	
+	;;
+
+	"RHEL"|"CentOS"|"Fedora")
+		echo -e "Downloading RPM Package for InfluxDB v1.8\n"
+		wget --continue -P /tmp/influxdb/ "https://dl.influxdata.com/influxdb/releases/influxdb-1.8.0.x86_64.rpm"
+
+		if [[ -f "/tmp/influxdb/influxdb-1.8.0.x86_64.rpm" ]]; then
+			echo -e "Installing InfluxDB v1.8.0\n"
+			yum localinstall /tmp/influxdb/influxdb-1.8.0.x86_64.rpm
+		else
+			echo -e "Error while downloading InfluxDB RPM Package\n"
+			exit 3
+		fi
+	;;
+
+	*)
+		echo -e "Unknown Distribution. Please Install Manually.\n"
+		exit 3
+	;;
+
+	esac    # --- end of case ---
+}
+
+
+function download_telegraf (distro) {
+
+	case distro in
+	"Debian"|"Ubuntu")
+		echo -e "Downloading Debian Package for Telegraf v1.14-5.1\n"
+		wget --continue -P /tmp/telegraf/ "https://dl.influxdata.com/telegraf/releases/telegraf_1.14.5-1_amd64.deb"
+		
+		if [[ -f "/tmp/telegraf/telegraf_1.14.5-1_amd64.deb" ]]; then
+			echo -e "Installing Telegraf v1.14.5-1\n"
+			dpkg -i /tmp/telegraf/telegraf_1.14.5-1_amd64.deb
+		else
+			echo -e "Error while downloading Telegraf Debian Package\n"
+			exit 3
+		fi	
+	;;
+
+	"RHEL"|"CentOS"|"Fedora")
+		echo -e "Downloading RPM Package for Telegraf v1.14.5-1\n"
+		wget --continue -P /tmp/telegraf/ "https://dl.influxdata.com/telegraf/releases/telegraf-1.14.5-1.x86_64.rpm"
+
+		if [[ -f "/tmp/telegraf/telegraf-1.14.5-1.x86_64.rpm" ]]; then
+			echo -e "Installing Telegraf v1.14.5-1\n"
+			yum localinstall /tmp/telegraf/telegraf-1.14.5-1.x86_64.rpm
+		else
+			echo -e "Error while downloading Telegraf RPM Package\n"
+			exit 3
+		fi
+	;;
+
+	*)
+		echo -e "Unknown Distribution. Please Install Manually.\n"
+		exit 3
+	;;
+
+	esac    # --- end of case ---
+}
+
+
+function download_grafana (distro) {
+
+	case distro in
+	"Debian"|"Ubuntu")
+		echo -e "Downloading Debian Package for Grafana v7.0.5\n"
+		apt-get install -y adduser libfontconfig1
+		wget --continue -P /tmp/grafana/ " https://dl.grafana.com/oss/release/grafana_7.0.5_amd64.deb"
+		
+		if [[ -f "/tmp/grafana/grafana_7.0.5_amd64.deb" ]]; then
+			echo -e "Installing Grafana v7.0.5\n"
+			dpkg -i /tmp/grafana/grafana_7.0.5_amd64.deb
+		else
+			echo -e "Error while downloading Grafana Debian Package\n"
+			exit 3
+		fi	
+	;;
+
+	"RHEL"|"CentOS"|"Fedora")
+		echo -e "Downloading RPM Package for Grafana v7.0.5\n"
+		wget --continue -P /tmp/grafana/ "https://dl.grafana.com/oss/release/grafana-7.0.5-1.x86_64.rpm"
+
+		if [[ -f "/tmp/grafana/grafana-7.0.5-1.x86_64.rpm" ]]; then
+			echo -e "Installing Grafana v7.0.5\n"
+			yum install /tmp/grafana/grafana-7.0.5-1.x86_64.rpm
+		else
+			echo -e "Error while downloading Telegraf RPM Package\n"
+			exit 3
+		fi
+	;;
+
+	*)
+		echo -e "Unknown Distribution. Please Install Manually.\n"
+		exit 3
+	;;
+
+	esac    # --- end of case ---
+}
+
 
 #-------------------------------------------------------------------------------
 #   STEP 1: Check if Script is running with Root Privileges
@@ -50,90 +165,15 @@ eval `cat $ENVFILE`
 
 echo -e " Found Distribution: $DISTRO\n"
 
+#-------------------------------------------------------------------------------
+#   STEP 3: Download Telegraf, InfluxDB, Grafana
+#-------------------------------------------------------------------------------
 
-case $DISTRO in
-	"Debian"|"Raspbian")
-		echo -e "Add InfluxData Repository to Sources List\n"
-		wget -qO - https://repos.influxdata.com/influxdb.key | apt-key add -
-		source /etc/os-release
-		echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/influxdb.list
+download_influxdb($DISTRO) && download_telegraf($DISTRO) && download_grafana($DISTRO)
 
-		echo -e "Add Grafana Repository to Sources List\n"
-		wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
-
-		echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list 
-
-		retval=$?
-		if [ $retval -ne 0 ]; then
-			echo -e "Unable to add the Telegraf/InfluxData/Grafana Repository in Sources List\n"
-			exit $retval
-		else
-			echo -e "Installing Telegraf, InfluxDB and Grafana\n"
-			apt-get update && apt-get install telegraf && apt-get install influxdb && apt-get install grafana
-		fi
-	;;
-
-	"Ubuntu")
-		echo -e "Add InfluxData Repository to Sources List\n"
-		wget -qO - https://repos.influxdata.com/influxdb.key | apt-key add -
-		source /etc/lsb-release
-		echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
-
-		echo -e "Add Grafana Repository to Sources List\n"
-		wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
-
-		add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
-
-		retval=$?
-		if [ $retval -ne 0 ]; then
-			echo -e "Unable to add the InfluxData Repository in Sources List\n"
-			exit $retval
-		else
-			echo -e "Installing Telegraf, InfluxDB and Grafana\n"
-			apt-get update && apt-get install telegraf && apt-get install influxdb && apt-get install grafana
-		fi
-	;;
-
-	"RHEL"|"CentOS"|"Fedora")
-		#WARNING: This section is not tested at all. The script might fail here!
-
-		echo -e "Adding InfluxData Repository to yum Repos\n"
-		cat <<- EOF > tee /etc/yum.repos.d/influxdb.repo
-		[influxdb]
-		name = InfluxDB - RHEL \$releasever
-		baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
-		gpgcheck = 1
-		enabled = 1
-		gpgkey = https://repos.influxdata.com/influxdb.key
-		EOF
-
-		echo -e "Adding Grafana Repository to yum Repos\n"
-		cat <<-EOF > tee /etc/yum.repos.d/grafana.repo
-		[grafana]
-		name=grafana
-		baseurl=https://packages.grafana.com/oss/rpm
-		repo_gpgcheck=1
-		enabled=1
-		gpgcheck=1
-		gpgkey=https://packages.grafana.com/gpg.key
-		sslverify=1
-		sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-		EOF
-
-		retval=$?
-		if [ $retval -ne 0 ]; then
-			echo -e "Unable to add Telegraf/InfluxData/Grafana Repository in Sources List\n"
-			exit $retval
-		else
-			yum install telegraf && yum install influxdb && yum install grafana
-		fi
-	;;
-
-	*)
-		echo -e "Unknown Distribution. Please Install Manually.\n"
-		exit 2
-	;;
-
-esac    # --- end of case ---
+if [ $? -ne 0 ]; then
+	echo -e "Error while downloading TIG stack\n"
+	exit 4
+fi
 
 exit
